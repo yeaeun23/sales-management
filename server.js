92cca721da4bd02ed232e53fb1a0134d2aadc28c
yeaeun23 <yeaeun23@naver.com>
2023-03-26 00:11:22 +0900

    수정

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.post(apiPrefix + '/login', (req, res) => {
             console.log("로그인 실패(3): " + id);
           }
           else {
-            let status = rows[0].status;
+            let status = 9;
             res.send({ 'code': 9, 'status': status });
             console.log("로그인 성공(9): " + id);
           }
